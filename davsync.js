688af1af89b922eca06374ca7bb191fec77e2fbc
Alex Ezell <alex.ezell@rackspace.com>
2015-10-16 16:13:02 -0400

    Added database for etags and hrefs
    Look in checker.js for ideas on comparing objects/arrays

--- davsync.js
+++ davsync.js
@@ -1,7 +1,9 @@
 var watch = require('watch'),
     request = require('request'),
     fs = require('fs'),
-    path = require('path');
+    path = require('path'),
+    low = require('lowdb');
+var db = low('db.json');
 
 exports.sync = function(username, password, local, remote) {
   watch.createMonitor(local, {
@@ -31,29 +33,33 @@ exports.sync = function(username, password, local, remote) {
   });
 
   function upl(f, stat) {
-    var filePath = path.relative(local, f);
-    console.log(remote + filePath);
+    var filePath = path.relative(local, f),
+        url = remote + filePath;
+    console.log(url);
     fs.createReadStream(f)
       .pipe(
         request({
           method: 'PUT',
-          url: remote + filePath,
+          url: url,
           auth: {
             user: username,
             pass: password
           }
         }, function(error, response, body) {
           console.log(response.headers.etag);
+          db('syncdb').push({ etag: response.headers.etag, href: url});
+          console.log(db('syncdb').find({etag: response.headers.etag}));
         })
       );
   }
 
   function mkdir(f) {
-    var filePath = path.relative(local, f);
-    console.log(remote + filePath);
+    var filePath = path.relative(local, f),
+        url = remote + filePath;
+    console.log(url);
     request({
       method: 'MKCOL',
-      uri: remote + filePath,
+      uri: url,
       auth: {
         user: username,
         pass: password
@@ -64,17 +70,20 @@ exports.sync = function(username, password, local, remote) {
   }
 
   function del(f) {
-    var filePath = path.relative(local, f);
-    console.log(remote + filePath);
+    var filePath = path.relative(local, f),
+        url = remote + filePath;
+    console.log(url);
     request({
       method: 'DELETE',
-      url: remote + filePath,
+      url: url,
       auth: {
         user: username,
         pass: password
       }
     }, function(error, response, body) {
-      console.log(response.headers.etag);
+      console.log(url);
+      db('syncdb').remove({href: url});
+      console.log(db('syncdb').find({href: url}));
     });
   }
 
